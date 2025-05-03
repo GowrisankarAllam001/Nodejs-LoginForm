002c092c1265a587db105d9f42a1de68f9ce256c
Allam GowriSankar <126642378+GowrisankarAllam001@users.noreply.github.com>
2025-05-03 14:18:48 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: 'Nodejs-LoginForm', // 👈 replace with your repo name
-  plugins: [react()],
-});
